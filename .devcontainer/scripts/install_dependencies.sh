9fbf49fec1951f9bc36310c05b6ce7130658ab17
andrelohmann <lohmann.andre@gmail.com>
2025-05-19 08:44:36 +0200

    fixed inventory

--- .devcontainer/scripts/install_dependencies.sh
+++ .devcontainer/scripts/install_dependencies.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "10.0.0.10 controller controller.os.lokal" >> /etc/hosts
-echo "10.0.0.11 compute1 compute1.os.lokal" >> /etc/hosts
-echo "10.0.0.12 compute2 compute2.os.lokal" >> /etc/hosts
+echo "10.0.10.10 controller controller.os.lokal" >> /etc/hosts
+echo "10.0.10.11 compute1 compute1.os.lokal" >> /etc/hosts
+echo "10.0.10.12 compute2 compute2.os.lokal" >> /etc/hosts
 
 # Install roles and collections
 ansible-galaxy role install -r requirements.yml --force
